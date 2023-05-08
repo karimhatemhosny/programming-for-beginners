c01654d0c46947804f573482751641b748324e17
kareem hatem <karimhatemhosny68@gmail.com>
2023-05-08 17:14:25 +0300

    added a footer

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,30 +1,30 @@
 const menuContent = `
 <ul>
-    <li><a href="../index.html">Home</a></li>
-    <li class="dropdown">
+    <li class='dd-list'><a href="../index.html">Home</a></li>
+    <li class="dropdown dd-list">
         <a href="javascript:void(0)" class="dropbtn">HTML</a>
         <div class="dropdown-content">
             <div class="row">
                 <div class="column">
                     <a href="label and input.html">Label and input</a>
-                    <a href="link.html">Links</a>
-                    <a href="lists.html">Lists</a>
-                    <a href="classes and ids.html">Classes and ids</a>
-                    <a href="divs and spans.html">Divs and Spans</a>
-                    <a href="comments.html">Comments</a>
+                    <a href="../html/link.html">Links</a>
+                    <a href="../html/lists.html">Lists</a>
+                    <a href="../html/classes and ids.html">Classes and ids</a>
+                    <a href="../html/divs and spans.html">Divs and Spans</a>
+                    <a href="../html/comments.html">Comments</a>
                 </div>
                 <div class="column">
-                    <a href="iframes.html">Iframes</a>
-                    <a href="headings.html">Headings</a>
-                    <a href="buttons.html">Buttons</a>
-                    <a href="text decoration.html">Text decoration</a>
-                    <a href="images and videos.html">Images and Videos</a>
-					<a href='pre.html'>Pre</a>
+                    <a href="../html/iframes.html">Iframes</a>
+                    <a href="../html/headings.html">Headings</a>
+                    <a href="../html/buttons.html">Buttons</a>
+                    <a href="../html/text decoration.html">Text decoration</a>
+                    <a href="../html/images and videos.html">Images and Videos</a>
+					<a href='../html/pre.html'>Pre</a>
                 </div>
             </div>
         </div>
     </li>
-    <li class="dropdown">
+    <li class="dropdown dd-list">
         <a href="javascript:void(0)" class="dropbtn">CSS</a>
         <div class="dropdown-content">
             <div class="column">
@@ -32,7 +32,7 @@ const menuContent = `
             </div>
         </div>
     </li>
-    <li class="dropdown">
+    <li class="dropdown dd-list">
         <a href="javascript:void(0)" class="dropbtn">How to</a>
         <div class="dropdown-content">
         <div class="column">
