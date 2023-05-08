f1a0a441d782b4c3a50e13964a2b1ae708284823
kareem hatem <karimhatemhosny68@gmail.com>
2023-05-08 21:37:51 +0300

    modified the links in the footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,14 +1,28 @@
 let footer = ` 
     <div id='container'>
         <div id='website'>
-            
+            <p>Programming for beginners</p>
             <ol id='footer-list'>
-                <li><a href='#'>a</a> <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
-                <li>a</li>
-                <li>a</li>
-                <li>a</li>
-                <li>a</li>
+                <li><a href='../index.html' target='_blank'>App</a> <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
+                <li><a href='https://github.com/kareem-hatem/programming-for-beginners' target='_blank'>GitHub</a> <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
             <ol>
+        </div>
+        <div id='more'>
+            <p>More</p>
+            <ol id='footer-list'>
+                <li><a href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=VpCqJXKBqpllDpmNBHfqdtgDTCvlcLDvcLvjhDjBTfQBzDlKKqNzGzcBGdgkZHsqVZnMlgB' target='_blank'>Contact</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
+            <ol>
+        </div>
+        <div id='karim'>
+            <p>Owner</p>
+            <ol id='footer-list'>
+                <li><a href='https://github.com/kareem-hatem' target='_blank'>GitHub profile</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
+                <li><a href='https://dev.to/kareemhatem' target='_blank'>Dev.to profile</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
+                <li><a href='https://replit.com/@kareem-hatem' target='_blank'>Replit profile</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
+            <ol>
+        </div>
+        <div id='copy'>
+            <p>Copyright &copy; 2023 <a href='https://github.com/kareem-hatem' target='_blank' id='name'>Karim Hatem</a></p>
         </div>
     </div>
 `; 
