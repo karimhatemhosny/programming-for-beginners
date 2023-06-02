11e9168527a2760befc6b50884a5681011731bda
kareem hatem <karimhatemhosny68@gmail.com>
2023-06-02 03:06:58 +0300

    added a rating webpage to send feedback

--- scripts/footer.js
+++ scripts/footer.js
@@ -12,6 +12,7 @@ let footer = `
             <ol id='footer-list'>
                 <li><a href='../main pages/contact.html' target='_blank'>Contact</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
                 <li><a href='../main pages/credits.html' target='_blank'>Credits</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
+                <li><a href='../main pages/rating.html' target='_blank'>Rate us</a><svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></li>
             <ol>
         </div>
         <div id='karim'>
